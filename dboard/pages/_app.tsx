1bd38186984097979ea7deeb10a1fdcc767ccd63
nkazlauskas <nadiakazlauskas@gmail.com>
2024-07-24 16:29:03 -0300

    Adding home layout

--- dboard/pages/_app.tsx
+++ dboard/pages/_app.tsx
@@ -1,14 +1,28 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
+import { createTheme, ThemeProvider } from "@mui/material";
 import Head from "next/head";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#01599b",
+    },
+    secondary: {
+      main: "#8ad3e0",
+    },
+  },
+});
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-        <title>Dashboard</title>
+        <title>Tablero de Monitoreo</title>
       </Head>
-      <Component {...pageProps} />
+      <ThemeProvider theme={theme}>
+        <Component {...pageProps} />
+      </ThemeProvider>
     </>
   );
 }
--- dboard/pages/index.tsx
+++ dboard/pages/index.tsx
@@ -1,11 +1,100 @@
-import { Typography } from "@mui/material";
+import Grid from "@mui/material/Grid";
+import Item from "@mui/material/ListItem";
+import Button from "@mui/material/Button";
+import DrawerMenu from "../components/drawer";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import Image from "next/image";
+
+const drawerWidth = 250;
 
 export default function Home() {
   return (
-    <main>
-      <div>
-        <Typography>Tablero de Monitoreo</Typography>
-      </div>
-    </main>
+    <>
+      <DrawerMenu />
+      <Box
+        sx={{
+          display: "flex",
+          width: { sm: `calc(100% - ${drawerWidth}px)` },
+          ml: { sm: `${drawerWidth}px` },
+          pr: 10,
+          pl: 10,
+        }}
+      >
+        <Grid container style={{ alignItems: "center" }}>
+          <Grid item xs={12} md={4} mt={{ xs: 5 }}>
+            <Item
+              style={{
+                textAlign: "center",
+                justifyContent: "center",
+              }}
+            >
+              <div>
+                <Image
+                  src="/short-term.png"
+                  alt="short-term"
+                  width={120}
+                  height={75}
+                ></Image>
+                <Typography align="center" sx={{ mt: 2 }}>
+                  <Button
+                    id="short-term"
+                    href="/shortTerm"
+                    variant="contained"
+                    size="large"
+                  >
+                    Pronóstico a Corto Plazo
+                  </Button>
+                </Typography>
+              </div>
+            </Item>
+          </Grid>
+          <Grid item xs={12} md={4} mt={{ xs: 5 }}>
+            <Item style={{ textAlign: "center", justifyContent: "center" }}>
+              <div>
+                <Image
+                  src="/long-term.png"
+                  alt="long-term"
+                  width={225}
+                  height={75}
+                ></Image>
+                <Typography align="center" sx={{ mt: 2 }}>
+                  <Button
+                    id="long-term"
+                    href="/longTerm"
+                    variant="contained"
+                    size="large"
+                  >
+                    Pronóstico a Largo Plazo
+                  </Button>
+                </Typography>
+              </div>
+            </Item>
+          </Grid>
+          <Grid item xs={12} md={4} mt={{ xs: 5 }}>
+            <Item style={{ textAlign: "center", justifyContent: "center" }}>
+              <div>
+                <Image
+                  src="/meteorological.png"
+                  alt="meteorological"
+                  width={90}
+                  height={75}
+                ></Image>
+                <Typography align="center" sx={{ mt: 2 }}>
+                  <Button
+                    id="meteorological"
+                    href="/meteorological"
+                    variant="contained"
+                    size="large"
+                  >
+                    Pronóstico Meteorológico
+                  </Button>
+                </Typography>
+              </div>
+            </Item>
+          </Grid>
+        </Grid>
+      </Box>
+    </>
   );
 }
